69757194557d48fcacab614739ca8d3936d9df07
Shannon Holmes <snholmes13@gmail.com>
2018-07-21 21:42:30 +0100

    add or remove cash test

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,12 @@ end
 def total_cash(sum)
   return sum[:admin][:total_cash]
 end
+
+def add_or_remove_cash(total_cash, amount)
+   total_cash[:admin][:total_cash] += amount
+  return total_cash
+end
+
+def
+  return
+end
